d31cf29b84b16ec8d9c87571009bc947a4494e3e
Alison Tamayo <aatamayo2@espe.edu.ec>
2026-01-18 20:03:08 -0500

    Sync final: Limpieza total y POMs corregidos

--- src/main/java/com/bancario/compensacion/dto/CatalogoErrorDTO.java
+++
@@ -1,9 +0,0 @@
-package com.bancario.compensacion.dto;
-
-@lombok.Getter
-@lombok.Setter
-@lombok.AllArgsConstructor
-public class CatalogoErrorDTO {
-    private String codigo;
-    private String descripcion;
-}
--- src/main/java/com/bancario/compensacion/repository/CatalogoErrorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bancario.compensacion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bancario.compensacion.model.CatalogoError;
-
-public interface CatalogoErrorRepository extends JpaRepository<CatalogoError, String> { }
